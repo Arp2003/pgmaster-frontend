fe13fb93029b6b6f5545f078880e487915938e0a
Arp2003 <136185997+Arp2003@users.noreply.github.com>
2026-05-08 11:46:01 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+
   images: {
     remotePatterns: [
       {
@@ -15,9 +16,6 @@ const nextConfig = {
         pathname: '/**',
       },
     ],
-  },
-  env: {
-    NEXT_PUBLIC_API_URL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:8000/api/v1',
   },
 }
 
